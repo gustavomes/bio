ce110ae88ae50846c95891cd055eea5e34ef8693
gustavomes <gustavomes@yahoo.com.br>
2022-03-29 19:06:36 -0300

    Entrega 2

--- src/components/Portiflio/index.js
+++ src/components/Portiflio/index.js
@@ -14,7 +14,7 @@ const Por = [
   {
     id: "2",
     name: "Checkpoint I - Front End II",
-    sub: "escolha seu campeão no LoL",
+    sub: "Escolha seu campeão no LoL",
     image:
       "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt8979808f7798ecf5/6216ee875fe07272a8a2447a/2021_Key_art.jpg",
       link: "https://gustavomes.github.io/frontEnd2_checkpoit1/ ",
@@ -33,7 +33,7 @@ const Por = [
   {
     id: "4",
     name: "Projeto Veggie",
-    sub: "app de produtos veganos",
+    sub: "App de produtos veganos",
     image:
       "https://veganbusiness.com.br/wp-content/uploads/2019/02/aplicativos-veganos.jpg",
       link: "https://github.com/gustavomes/Checkpoint02-FrontendII",
@@ -65,9 +65,9 @@ const logo = "https://cdn.geckoandfly.com/wp-content/uploads/2018/03/yosemite-59
               <ul className="list-por">
                 {Por.map(({ image, name, sub, link }) => (
                   <a href={link} target="_blank">
-                    <li className="item-list-projects-por-container">
+                    <li className="item-list-projects-por-container" >
                       <img src={image} className="usandoImagemPor" />
-                      <h5 className="name-project-por" id="centered"> {name} </h5>
+                      <h5 className="name-project-por" id="centered" > {name}</h5>
                       <h2 className="name-project-por" id="centered2"> {sub} </h2>
                     </li>
                       
